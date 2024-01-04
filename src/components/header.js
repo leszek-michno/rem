6b3466723966e9b16924ffcd5f5068b977fed67a
Leszek Michno <leszek.michno@gmail.com>
2024-01-04 10:42:08 +0100

    inwent2023

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <header>
-            <h2>Remanent Pub Propaganda 30.11.2023</h2>
+            <h2>Inwentaryzacja Pub Propaganda 31.12.2023</h2>
             <p>Lem-Son Sp. z o.o.</p>
             <hr />
         </header>
